9653cc527ef8902b9b26463fe12e78e11a43a3f3
Bhadresh Pithwa <bhadresh@tuvoc.com>
2024-01-22 16:47:09 +0530

    password error ui fixed

--- src/Components/CreatePassword.js
+++ src/Components/CreatePassword.js
@@ -10,19 +10,21 @@ function CreatePassword({ register, errors, formStyle }) {
   return (
     <div className={Style.passwordContainer}>
       <div className={formStyle.fieldHeading}>Create a password *</div>
-      <div className={Style.inputDiv}>
-        <InputComponent
-          type={showPassword ? "text" : "password"}
-          placeholder={"Password"}
-          register={register}
-          inputRef={"password"}
-          name={"password"}
-          className={formStyle.inputField}
-        />
-        <PasswordEyeComponent
-          showStatus={showPassword}
-          setShowStatus={setShowPassword}
-        />
+      <div>
+        <div className={Style.inputDiv}>
+          <InputComponent
+            type={showPassword ? "text" : "password"}
+            placeholder={"Password"}
+            register={register}
+            inputRef={"password"}
+            name={"password"}
+            className={formStyle.inputField}
+          />
+          <PasswordEyeComponent
+            showStatus={showPassword}
+            setShowStatus={setShowPassword}
+          />
+        </div>
         {errors.password &&
           (errors.password.type === "required" ||
             errors.password.type === "min" ||
@@ -32,19 +34,21 @@ function CreatePassword({ register, errors, formStyle }) {
             </div>
           )}
       </div>
-      <div className={Style.inputDiv}>
-        <InputComponent
-          type={showCPassword ? "text" : "password"}
-          placeholder={"Confirm Password"}
-          register={register}
-          inputRef={"confirmpassword"}
-          name={"confirmpassword"}
-          className={formStyle.inputField}
-        />
-        <PasswordEyeComponent
-          showStatus={showCPassword}
-          setShowStatus={setShowCPassword}
-        />
+      <div>
+        <div className={Style.inputDiv}>
+          <InputComponent
+            type={showCPassword ? "text" : "password"}
+            placeholder={"Confirm Password"}
+            register={register}
+            inputRef={"confirmpassword"}
+            name={"confirmpassword"}
+            className={formStyle.inputField}
+          />
+          <PasswordEyeComponent
+            showStatus={showCPassword}
+            setShowStatus={setShowCPassword}
+          />
+        </div>
         {errors.confirmpassword &&
           (errors.confirmpassword.type === "required" ||
             errors.confirmpassword.type === "oneOf") && (
