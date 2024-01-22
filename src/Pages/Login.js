7473ce9062d748750bf302cb49213dd7db347dc3
Bhadresh Pithwa <bhadresh@tuvoc.com>
2024-01-22 15:46:10 +0530

    Login design changes applied

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,6 +3,7 @@ import style from "../Styles/Login.module.css";
 import LoginForm from "./LoginForm";
 import SigninImage from "../images/Login.svg";
 import { useScrollToTop } from "../hooks/useScrollToTop";
+import HeaderComponent from "../Components/HeaderComponent";
 
 const Login = () => {
   const scrollToTop = useScrollToTop();
@@ -11,16 +12,25 @@ const Login = () => {
     scrollToTop(); // Automatically scroll to top when the component is mounted
   }, []);
 
+  // return (
+  //   <div className={style.container}>
+  //     <div className={style.mainDiv}>
+  //       <div className={style.leftDiv}>
+  //         <LoginForm />
+  //       </div>
+  //       <div className={style.rightDiv}>
+  //         <img src={SigninImage} alt={"SignIn"} />
+  //       </div>
+  //     </div>{" "}
+  //   </div>
+  // );
+
   return (
-    <div className={style.container}>
-      <div className={style.mainDiv}>
-        <div className={style.leftDiv}>
-          <LoginForm />
-        </div>
-        <div className={style.rightDiv}>
-          <img src={SigninImage} alt={"SignIn"} />
-        </div>
-      </div>{" "}
+    <div className={style.mainDiv}>
+      <HeaderComponent />
+      <div className={style.formContainer}>
+        <LoginForm />
+      </div>
     </div>
   );
 };
