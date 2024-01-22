083f2671a8db9142ca0af9ca63a3036d31cb1a2a
Bhadresh Pithwa <bhadresh@tuvoc.com>
2024-01-22 10:26:06 +0530

    Common header component added

---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import HeaderUserComponent from "./HeaderUserComponent";
+import logo from "../images/logo.svg";
+
+const HeaderComponent = ({ HeaderStyle }) => {
+  const navigate = useNavigate();
+  const goToHomePage = () => {
+    navigate("/");
+  };
+  return (
+    <div className={HeaderStyle.navHeader}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "7px",
+          flexGrow: "1",
+          cursor: "pointer",
+        }}
+        onClick={goToHomePage}
+        aria-hidden="true"
+      >
+        <img src={logo} alt="logo" className={HeaderStyle.oneLogo} />
+        <h2 className={HeaderStyle.brand}>NE</h2>
+      </div>
+
+      <HeaderUserComponent
+        headerClass={HeaderStyle}
+        calledFromClass="eventDetailHeader"
+      />
+    </div>
+  );
+};
+
+export default HeaderComponent;
