e6e5d8c6393120b41ad6b53e86ff334d50a39e38
Bhadresh Pithwa <bhadresh@tuvoc.com>
2024-02-01 12:48:41 +0530

    Added for status 403

--- src/api/api.js
+++ src/api/api.js
@@ -9,6 +9,7 @@ export const axiosClient = axios.create({
     return (
       (status >= 200 && status < 300) ||
       status === 401 ||
+      status === 403 ||
       status === 409 ||
       status === 500
     ); // default
