05f8b7154377eaaca0f1ad09a1b37e69a98c0c7f
Bhadresh Pithwa <bhadresh@tuvoc.com>
2024-02-23 17:42:45 +0530

    New date fiter selector applied for ui changes missing file add

---
+++ src/Components/CalendarFilterComponent.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import calendarIcon from "../images/calendar-icon.png";
+import downArrowIcon from "../images/down-arrow.png";
+import calendarStyle from "../Styles/Calendar.module.css";
+import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "../Styles/calendarstyle.css";
+
+function CalendarFilterComponent({
+  startDate,
+  endDate,
+  setStartDate,
+  setEndDate,
+  handleSearch,
+}) {
+  const [dateRange, setDateRange] = useState([startDate, endDate]);
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+  const toggleDatePicker = () => {
+    setIsDatePickerOpen(!isDatePickerOpen);
+  };
+
+  const formatDate = (date) => {
+    return moment(date).format("MMMM DD, YYYY");
+  };
+
+  const handleDateChange = (e) => {
+    setDateRange(e);
+  };
+
+  const handleCalendarClose = (e) => {
+    setIsDatePickerOpen(false);
+  };
+
+  useEffect(() => {
+    setStartDate(dateRange[0]);
+    setEndDate(dateRange[1]);
+  }, [dateRange]);
+
+  return (
+    <div className={calendarStyle.calendarContainer}>
+      <div className={calendarStyle.dateContainer} onClick={toggleDatePicker}>
+        <span className={calendarStyle.calndarIcon}>
+          <img
+            src={calendarIcon}
+            alt="calendar"
+            className={calendarStyle.calndarIconImg}
+          />
+        </span>
+        <span className={calendarStyle.dateText}>
+          {formatDate(dateRange[0])} - {formatDate(dateRange[1])}
+        </span>
+        <span>
+          <img src={downArrowIcon} alt="down" />
+        </span>
+      </div>
+      <div className={calendarStyle.hiddenDateSection}>
+        <DateRangePicker
+          onChange={handleDateChange}
+          value={dateRange}
+          clearIcon={null}
+          isOpen={isDatePickerOpen}
+          onCalendarClose={handleCalendarClose}
+          minDate={new Date()}
+          maxDetail="month"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CalendarFilterComponent;
