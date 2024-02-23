80456e699e4fa28dd2b150add6d6c99075ed4871
Bhadresh Pithwa <bhadresh@tuvoc.com>
2024-02-23 18:08:37 +0530

    Common Nav header UI added

--- src/Components/EventFilterComponent.js
+++ src/Components/EventFilterComponent.js
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 import HeaderUserComponent from "./HeaderUserComponent";
 import { IoMdClose } from "react-icons/io";
 import CalendarFilterComponent from "./CalendarFilterComponent";
+import NavHeaderComponent from "./NavHeaderComponent";
 
 function EventFilterComponent({
   startDate,
@@ -35,38 +36,10 @@ function EventFilterComponent({
 
   return (
     <>
-      <div className={Style.navHeader}>
-        <div className={Style.filterContainer}>
-          <img src={searchIcon} alt="search.." />
-          <input
-            type="text"
-            className={Style.filter}
-            placeholder="Search"
-            onChange={getFilterOutPut}
-            // disabled={!filter}
-            value={filterData}
-          />
-          {filterData ? (
-            <span onClick={handleClearSearch} className={Style.clearIcon}>
-              <IoMdClose />
-            </span>
-          ) : (
-            <span className={Style.clearIcon}></span>
-          )}
-        </div>
-
-        <div className={Style.upperHeader}>
-          <div className={Style.brandText} onClick={goToHomePage}>
-            <img src={logo} alt="logo" className={Style.oneLogo} />
-            <div>
-              <div className={Style.brand}>NE</div>
-              <div className={Style.subBrand}>Boulder, CO</div>
-            </div>
-          </div>
-
-          <HeaderUserComponent headerClass={Style} />
-        </div>
-      </div>
+      <NavHeaderComponent
+        setFilterData={setFilterData}
+        filterData={filterData}
+      />
       <div className={Style.filterdiv}>
         <CalendarFilterComponent
           startDate={startDate}
---
+++ src/Components/NavHeaderComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+// import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Style from "../Styles/NavHeader.module.css";
+import logo from "../images/logo.svg";
+import searchIcon from "../images/Search.svg";
+import { useNavigate } from "react-router-dom";
+import HeaderUserComponent from "./HeaderUserComponent";
+import { IoMdClose } from "react-icons/io";
+
+function NavHeaderComponent({ setFilterData, filterData }) {
+  const navigate = useNavigate();
+
+  const getFilterOutPut = (e) => {
+    setFilterData(e.target.value);
+  };
+
+  const goToHomePage = () => {
+    navigate("/");
+  };
+
+  const handleClearSearch = () => {
+    setFilterData("");
+  };
+
+  return (
+    <div className={Style.navHeader}>
+      <div className={Style.filterContainer}>
+        <img src={searchIcon} alt="search.." />
+        <input
+          type="text"
+          className={Style.filter}
+          placeholder="Search"
+          onChange={getFilterOutPut}
+          value={filterData}
+        />
+        {filterData ? (
+          <span onClick={handleClearSearch} className={Style.clearIcon}>
+            <IoMdClose />
+          </span>
+        ) : (
+          <span className={Style.clearIcon}></span>
+        )}
+      </div>
+
+      <div className={Style.upperHeader}>
+        <div className={Style.brandText} onClick={goToHomePage}>
+          <img src={logo} alt="logo" className={Style.oneLogo} />
+          <div>
+            <div className={Style.brand}>NE</div>
+            <div className={Style.subBrand}>Boulder, CO</div>
+          </div>
+        </div>
+
+        <HeaderUserComponent headerClass={Style} />
+      </div>
+    </div>
+  );
+}
+
+export default NavHeaderComponent;
